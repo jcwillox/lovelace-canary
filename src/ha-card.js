2d2f28d0b41badf028f166594fd2d3cc74650b49
Josh Willox <39120423+jcwillox@users.noreply.github.com>
2020-07-05 20:46:35 +1000

    Fix transition animations, add `canary_style` and `canary_theme` extensions

--- src/ha-card.js
+++ src/ha-card.js
@@ -1,21 +1,37 @@
 import { fireEvent } from "card-tools/src/event.js";
 import { extensionEnabled, findConfig } from "./utils";
+import { applyTheme } from "./styles";
+import { NO_CARD_STYLE } from "./const";
+import { hass } from "card-tools/src/hass";
 
 customElements.whenDefined("ha-card").then(() => {
   const HaCard = customElements.get("ha-card");
   const firstUpdated = HaCard.prototype.firstUpdated;
 
-  HaCard.prototype.firstUpdated = function() {
+  HaCard.prototype.firstUpdated = function () {
     firstUpdated.call(this);
     const config = findConfig(this);
-    if (
-      config &&
-      config.no_card === true &&
-      extensionEnabled(config, "no_card")
-    ) {
-      this.style.boxShadow = "none";
-      this.style.background = "none";
+    if (!config) return;
+
+    // ha-card elements have default transition that we need to temporarily disable.
+    this.style.transition = "none";
+
+    if (config.no_card === true && extensionEnabled(config, "no_card")) {
+      Object.assign(this.style, NO_CARD_STYLE);
     }
+
+    if (config.canary_style && extensionEnabled(config, "canary_style")) {
+      Object.assign(this.style, config.canary_style);
+    }
+
+    if (config.canary_theme && extensionEnabled(config, "canary_theme")) {
+      applyTheme(this, hass().themes, config.canary_theme);
+    }
+
+    // flush css.
+    this.offsetHeight;
+    // restore transition.
+    this.style.transition = "";
   };
 
   fireEvent("ll-rebuild", {});
